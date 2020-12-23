8e925af9db08b1726bb9ba6ef46242e4340b3dc4
sa931 <76529383+sa931@users.noreply.github.com>
2020-12-23 22:00:34 +0000

    start

---
+++ GroupProjectCode.cpp
@@ -0,0 +1,35 @@
+//function which will return an integer value of the heart rate, this will be called every few milli second  
+int heartSensor()
+{
+}
+
+//foxgen sensor function, retuen integer of oxgyen value
+int oxygenSensor()
+{
+}
+
+// function to project gps system to screen
+void gps()
+{
+}
+
+
+int main()
+{
+	if (button1 =! 0)
+	{
+		oxygenSensor();
+	}
+
+	else if (button2 =! 0)
+	{
+		heartSensor();
+	}
+
+	else if (button3 = !0)
+	{
+		gps();
+	}
+
+}
+
